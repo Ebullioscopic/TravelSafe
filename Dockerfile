c33d37f7e1e1860a2bf4aa733fca6c227c7343f0
ajdjyt <ajdjking@gmail.com>
2023-04-12 18:58:27 +0530

    Testing sentiment analysis

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM tensorflow/tensorflow:latest-gpu
+ENV NLTK_DATA=/usr/local/share/ntlk_data
 RUN groupadd -r swuser -g 433 && \
     useradd -u 431 -r -g swuser -s /sbin/nologin -c "Docker image user" swuser
 COPY build /main/
 USER root
-RUN chown -R swuser:swuser /main/*
+RUN chown -R swuser:swuser /main/* 
+RUN mkdir -p /home/swuser
+RUN chown -R swuser:swuser /home/swuser
 RUN pip install -r /main/requirements.txt
-USER swuser
+#COPY punkt.zip /main/punkt.zip
+#RUN python3 -m nltk.downloader punkt
+#RUN curl https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt.zip -o /main/punkt.zip
+#RUN mkdir -p /usr/local/share/ntlk_data/models/punkt
+#RUN unzip /main/punkt.zip -d /usr/local/share/ntlk_data/models/punkt
+USER swuser 
 CMD python3 /main/main.py
--- build/main.py
+++ build/main.py
@@ -1,6 +1,59 @@
 import tensorflow as tf
-import pandas as pd
-import numpy as np
-import math
-from xgboost import XGBClassifier
-print("hello world")
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+import re
+import string
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+def preprocess_text(text):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove numbers and punctuation
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    
+    # Tokenize the text
+    tokens = word_tokenize(text)
+    
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if not word in stop_words]
+    
+    # Join the tokens back into a string
+    text = ' '.join(tokens)
+    
+    return text
+
+# Load the IMDB dataset
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000)
+
+# Pad the sequences to a fixed length
+x_train = sequence.pad_sequences(x_train, maxlen=500)
+x_test = sequence.pad_sequences(x_test, maxlen=500)
+
+# Define the RNN model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=10000, output_dim=32),
+    tf.keras.layers.SimpleRNN(units=32),
+    tf.keras.layers.Dense(units=1, activation='sigmoid')
+])
+
+# Compile the model
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5, batch_size=128, validation_split=0.2)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(x_test, y_test)
+
+# Predict the sentiment of new text data
+text = "This is a great movie"
+sentiment = model.predict(preprocess_text(text))
+if sentiment > 0.5:
+    print("Positive sentiment")
+else:
+    print("Negative sentiment")
